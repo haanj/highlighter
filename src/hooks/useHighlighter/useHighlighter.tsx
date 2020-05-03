9b93fc607f5bdb154ec6008ceced2a88b8de680b
Joshua Haan <j.haan@f5.com>
2020-05-03 13:44:14 -0700

    cleans up useHighlighter

--- src/hooks/useHighlighter/useHighlighter.tsx
+++ src/hooks/useHighlighter/useHighlighter.tsx
@@ -2,30 +2,23 @@ import React from 'react'
 import nlp from 'compromise'
 
 export const useHighlighter = (text: string, highlights: string[] = []) => {
-  const highlighted = new Set(highlights)
-
-  const doc = nlp(text)
-
   const renderText = () => {
-    const children: React.ReactElement[] = []
-
-    const renderTerm = (term: nlp.Term) => {
-      const displayTerm = highlighted.has(term.clean || '')
-        ? <span data-testid="highlight">{term.text}</span>
-        : term.text
-      children.push((
-        <React.Fragment
-          key={term.id}
-        >{term.pre}{displayTerm}{term.post}</React.Fragment>
-      ))
-    }
-
-    doc
+    const terms = nlp(text)
       .termList()
-      .forEach(renderTerm)
-
-    return children
+      .map(renderTerm)
+    
+    return <>{terms}</>
   }
 
-  return <>{renderText()}</>
+  const renderTerm = (term: nlp.Term) => {
+    const displayTerm = highlights.includes(term.clean || '')
+      ? <span data-testid="highlight">{term.text}</span>
+      : term.text
+
+    return <React.Fragment
+        key={term.id}
+      >{term.pre}{displayTerm}{term.post}</React.Fragment>
+  }
+
+  return renderText()
 }
